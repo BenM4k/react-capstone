03ec2929bbb7412d55d9f48af0c1ada966dff393
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-08-03 07:32:39 +0200

    routes and slices tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn/i);
+test('renders App.js', () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+  const linkElement = screen.getByText(/Air Quality/);
   expect(linkElement).toBeInTheDocument();
 });
